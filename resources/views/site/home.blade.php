498498be2011ddbb1e95cfc9371d47e5a6cc31b5
Lais Midori Osaka <laismidori51@gmail.com>
2023-02-09 22:27:26 -0300

    atualização da home

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,49 +3,80 @@
 @section('titulo', 'Início')
 
 @section('conteudo')
-<h1 class="text-primary text-center mt-5">Projeto Chronos</h1>
-    <p>Página em Desenvolvimento... por Lais</p>
 
-<div class="row">
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="text-center fs-1">CHRONOS</h2>
+                <div class="col-md-6 mx-auto">
+                    <p class="text-center">
+                       Empresa capacitada para atender as suas necessitades.
+                    </p>
+                </div>
 
-<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <form method="get" action="">
+                    <div class="col-md-6 mx-auto">
+                        <input type="text" class="form-control is-invalid" name="nome" id="nome"
+                            placeholder="Procurar projeto">
+                    </div>
 
-    <h3 class="text-center mt-5 mb-3">Acessar</h3>
-    <div class="text-center mb-5">
-        <img src="assets/img/logoChronos.png" alt="" width="180">
+                </form>
+
+            </div>
+
+        </div>
+
+        <div class="row g-4">
+            <div class="col-md-12">
+
+            </div>
+            <div class="col-md-9">
+                <img src="/img/cards/blogger-2838945_1280.jpg" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-3 ">
+                <h3 class="mt-1">
+                    Gestão de Projeto para Instituições Financeiras
+                </h3>
+                <p>
+                   Obtenção de um conteúdo onde totalmente integrado a sistuações em que as Empresas devem estar atentas a cada passo dado para uma boa geração de renda e produção rentável, tendo em vista toda e qualquer situação, para que não ocorra erros futuros.
+                </p>
+
+                <a href="" class="btn btn-primary">Ver Completo</a>
+
+            </div>
+
+            <div class="col-md-6">
+                <img src="/img/cards/despaired-2261021_1280.jpg" alt="" class="img-fluid">
+
+                <h3 class="mt-1">Gestão de Projeto Hospitalares </h3>
+
+                <p class="mt-3">O assunto em questão sugere uma ampla visão do contexto, pois iremos tratar principalmente de questões unilaterais de cada etapa para que possa ser assim avalida de forma complexa e objetiva até o exato ponto de cada item citado.</p>
+
+                    <a href="" class="btn btn-primary">Ver Completo</a>
+            </div>
+            <div class="col-md-6">
+                <img src="/img/cards/entrepreneur-593371_1280.jpg" alt="" class="img-fluid">
+
+                <h3 class="mt-1">Gestão de Projeto Organizacionais</h3>
+
+                <p class="mt-3">De forma ampla iremos abordar todas as etapas onde será ressaltado questões em pode ser efertuado ajustes e formas de decisões que podem serem tomadas como base para as mais diversas situações.</p>
+
+                    <a href="" class="btn btn-primary">Ver Completo</a>
+            </div>
+        </div>
+
+            <div class="mt-5 col-md-2 mx-auto">
+                <a href="" class="btn btn-warning btn-lg">Ver Todos</a>
+            </div>
+
+
+
+
+
+
+
+
     </div>
 
-{{--
-    <div class="alert alert-danger text-center">
-        Usuário ou senha inválido!
-    </div> --}}
 
-    <form action="#" method="post" class="row g-3">
-        <div class="col-12">
-            <label for="email">E-mail</label>
-            <input type="email" id="email" name="email" value="" class="form-control" placeholder="Digite seu E-mail" autofocus>
-
-        </div>
-
-        <div class="col-12">
-            <label for="senha">Senha</label>
-            <input type="password" id="senha" name="senha" value="" class="form-control" placeholder="Digite sua senha">
-        </div>
-
-
-        <div class="col-12 mt-3 mb-3 form-check">
-            <input type="checkbox" name="manterLogado" class="custom-control-input" id="lembrame">
-            <label class="custom-control-label" for="lembrame">Lembra-me</label>
-        </div>
-
-
-        <div class="col-12">
-            <button class="btn btn-lg btn-primary w-100" type="submit">Entrar</button>
-        </div>
-
-    </form>
-
-</div>
-
-</div>
 @endsection
