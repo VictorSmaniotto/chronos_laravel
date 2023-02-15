70605c7521409060da9bbd840299d6bce1772890
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-02-14 22:13:54 -0300

    iniciei o crud de projetos

--- app/Http/Controllers/Admin/ProjetoController.php
+++ app/Http/Controllers/Admin/ProjetoController.php
@@ -2,82 +2,74 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Projeto;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProjetoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        return view('admin.projetos.index');
+        $projeto = Projeto::all();
+        return view('admin.projetos.index', ['projeto' => $projeto]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        $projeto = new Projeto();
+        return view('admin.projetos.cadastrar', ['projeto' => $projeto]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nome_projeto' => 'required',
+            'descricao' => 'required',
+            'objetivo' => 'required',
+            'data_criacao' => 'required',
+            'situacao' => 'required',
+            'curso_id' => 'required',
+            'categoria_id' => 'required',
+        ]);
+
+        try {
+            $projeto = new Projeto;
+            $projeto->nome_projeto = $request->nome_projeto;
+            $projeto->descricao = $request->descricao;
+            $projeto->objetivo = $request->objetivo;
+            $projeto->data_criacao = $request->data_criacao;
+            $projeto->situacao = $request->situacao;
+            $projeto->curso_id = $request->curso_id;
+            $projeto->categoria_id = $request->categoria_id;
+            $projeto->save();
+
+            return redirect()->route('admin.projetos.index')->with('sucesso', 'Curso cadastrado com sucesso!');
+        } catch (\Exception $e) {
+            return redirect()->back()->withInput()->with('erro', 'Ocorreu um erro ao cadastrar, por favor tente novamente!');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
